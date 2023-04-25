7dc991e3a88c63c4597adeb9225a79b20b10f220
prettyturtle <unifconti@gmail.com>
2023-04-25 22:27:56 +0900

    Initial Commit

---
+++ SwiftUI_Tutorial1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI_Tutorial1
+//
+//  Created by yc on 2023/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Tutorial1/SwiftUI_Tutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tutorial1App.swift
+//  SwiftUI_Tutorial1
+//
+//  Created by yc on 2023/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
